9acd1ededa1ac8c15404822d77ee05de489b80c1
Iryna Moiseieva <iriyamois@gmail.com>
2025-08-05 13:36:57 +0300

    feat: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
-};
+function reverse(number) {
+  const isNegative = number < 0;
+  const reversed = Math.abs(number)
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+    
+  return isNegative ? -Number(reversed) : Number(reversed);
+}

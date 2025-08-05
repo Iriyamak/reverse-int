8e651d9edfd2996f56f7982ecdbc3330fda4237f
Iryna Moiseieva <iriyamois@gmail.com>
2025-08-05 13:38:36 +0300

    fix: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 function reverse(number) {
   const isNegative = number < 0;
-  const reversed = Math.abs(number)
-    .toString()
-    .split('')
-    .reverse()
-    .join('');
-    
+  const reversed = Math.abs(number).toString().split('').reverse().join('');
   return isNegative ? -Number(reversed) : Number(reversed);
 }
